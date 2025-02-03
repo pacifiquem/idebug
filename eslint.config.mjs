44af832a9a6699d11eb31ee01d9d9a7203216a69
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2025-02-03 13:03:22 +0200

    chore: config for first proof-of-concept

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,16 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tseslint.config({
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }),
   eslintPluginPrettierRecommended,
 ];
